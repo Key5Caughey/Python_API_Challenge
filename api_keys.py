107135b693e4f8a684e72cfc302d30e2d750a771
Key <key5caughey@gmail.com>
2024-04-22 17:26:28 -0400

    updating python challenge file

---
+++ api_keys.py
@@ -0,0 +1,13 @@
+# OpenWeatherMap API Key
+weather_api_key = "059cc001a7c658700b18e0b9bc5fa219"
+
+# Geoapify API Key
+geoapify_key = "6b147005-170b-4419-81d2-3ff3ca8f5ede"
+
+# Census data API Key
+census_data_api_key = "a59c63ebf2b4f373f3931812b923df523dc85b6b"
+
+# OMDb API key
+omdbapi_apikey = "6b147005-170b-4419-81d2-3ff3ca8f5ede"
+
+#omdbapi.com/apikey.aspx?VERIFYKEY
